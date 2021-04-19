0c463d3708f68e4ecc5bc5549ded6126391adfe4
oluwadare <wintan1418@gmail.com>
2021-04-19 15:36:35 +0100

    create form for user profile

--- app/views/users/copy.rb
+++ app/views/users/copy.rb
@@ -1,22 +1,24 @@
-<div class='bg-white w-75 border rounded-border p-3 my-4 color-light-grey'>
-   <h2 class='text-blue-bold py-3 borderb-grey'>All <%= @user.full_name %> Opinions</h2>
-   <div class='d-flex flex-column'>
-     <% @opinions.each do |opi| %>
-      <div class='d-flex my-2'>
-        <%= user_photo(opi) %>
-        <div class='d-flex flex-column w-100'>
-          <div class='d-flex justify-content-between'>
-            <span class='font-weight-bold text-dark'>
-              <%= opi.user.username %>
-            </span>
-            <div class='mx-2'>
-              <%= user_edit_destroy_retweet(opi) %>
-            </div>
-          </div>
-          <span><%= opi.text %></span>
-          <span class='copied-opi'><%= user_copied_info(opi) %></span>
-        </div>
-      </div>
-     <% end %> 
-   </div>
+<div class='d-flex flex-column w-100'>
+  <div class='d-flex justify-content-center align-items-center flex-column flex-md-row'>
+    <%= link_follow(current_user, @user) %>
+    <%= profile_photo(@user) %>
+    <%= link_unfollow(@user)%>
+  </div>
+  <span class='align-self-center bold-name-right text-dark text-center mt-1'><%= @user.full_name %></span>
+  <span class='align-self-center username text-center color-light-grey'>@<%= @user.username %></span>
+  
+  <div class='d-flex flex-column flex-md-row'>
+    <div class='border flex-grow-1'>
+      <span class='text-center d-block font-weight-bolder'><%= @user.opinions.count %></span>
+      <span class='text-center d-block bold-flw color-light-grey'>Opinions</span>
+    </div>
+    <div class='border flex-grow-1'>
+      <span class='text-center d-block font-weight-bolder'><%= @user.followers.count %></span>
+      <span class='text-center d-block bold-flw color-light-grey'>Following</span>
+    </div>
+    <div class='border flex-grow-1'>
+      <span class='text-center d-block font-weight-bolder'><%= @user.followeds.count %></span>
+      <span class='text-center d-block bold-flw color-light-grey'>Followers</span>
+    </div>
+  </div>
 </div>
