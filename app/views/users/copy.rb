2f74bbcd9dcd5eefb156dc4f7fd3749ff5ca4ec8
oluwadare <wintan1418@gmail.com>
2021-04-20 12:00:17 +0100

    add user validations for opinion and scope

--- app/views/users/copy.rb
+++ app/views/users/copy.rb
@@ -1,38 +1,9 @@
-class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true, format: { without: /[<>]/, message: "symbols '<' and '>' are invalid for username" }
-  validates :full_name, presence: true
-  has_many :opinions, dependent: :delete.all
-  has_many :followers, class_name: 'Following', foreign_key: 'follower_id', dependent: :destroy
-  has_many :followeds, class_name: 'Following', foreign_key: 'followed_id', dependent: :destroy
-  has_many :follows, through: :followers, source: :followed
-  has_many :followds, through: :followeds, source: :follower
+class Opinion < ApplicationRecord
+  validates :text, presence: true
+  belongs_to :user
+  belongs_to :copied, class_name: 'User', foreign_key: 'copied_id', optional: true
 
-  def followeds_opinions
-    ids = follows.select(:id).ids
-    ids << id
-    Opinion.ordered_opinion.include_user_copied.user_filter_Opinion(User.user_and_following(ids))
-  end
-
-  def who_follow
-    ids = follows.select(:id).ids
-    ids << id
-    User.ordered_users.user_who_follow(ids)
-  end
-
-  def unfollow(user)
-    follows.destroy(user)
-  end
-
-  def copy_opi(opi)
-    copy_opinion = if opi.copied_id.nil? || opi.created_at != opi.updated_at
-                     opinions.build(text: opi.text, copied_id: opi.user_id)
-                   else
-                     opinions.build(text: opi.text, copied_id: opi.copied_id)
-                   end
-    copy_opinion.save
-  end
-
-  scope :ordered_users, -> { order(created_at: :desc) }
-  scope :user_and_following, ->(ids) { where(id: ids) }
-  scope :user_who_follow, ->(ids) { where.not(id: ids) }
+  scope :ordered_opinion, -> { order(created_at: :desc) }
+  scope :include_user_copied, -> { includes(:user, :copied) }
+  scope :user_filter_Opinion, ->(user) { where(user: user) }
 end
