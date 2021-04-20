df0b079b69a6e958b16b08968c7538450f5b4bc5
oluwadare <wintan1418@gmail.com>
2021-04-20 13:34:43 +0100

    add logics for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+helper_method :current_user
+
+def current_user
+    if_session[:user_id]
+    @current_user ||= user.find(session[:user_id])
+else
+    @current_user = nil
 end
+end
+
+def authenticate_user!
+return unless session[:user_id].nil?
+flash[:alert] = 'Please log in before you continue'
+redirect_to root_url
+
+end
+end
+
--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,2 +1,29 @@
 class FollowingsController < ApplicationController
+    before_action :set_user, only: %i[create destroy]
+    before_action :authenticate_user!
+
+def create
+@following = Following.new
+if @following.build_saving(@user,current_user)
+    flash[:notice] = 'Successfully following #{@user.username}'
+else
+    flash[:alert] = 'An error occured in following #{@user.username}'
+end
+redirect_to request.referer
+
+def destroy
+current_user.unfollow(@user)
+flash[:notice] = 'unfollowing #{@user.username}'
+redirect_to request.referer
+
+end
+private
+
+def set_user
+@user = user.find(params[:id])
+end
+
+def Following_params
+    params.fetch(:following,{})
+end
 end
