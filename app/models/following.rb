0a71b5d7ea8b36dc354bba249959a85b4f2466e4
oluwadare <wintan1418@gmail.com>
2021-04-20 11:39:51 +0100

    add new file to model

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,17 +1,2 @@
-  
 class Following < ApplicationRecord
-    validates :follower, presence: true
-    validates :followee, presence: true
-    belongs_to :follower, class_name: 'User'
-    belongs_to :followed, class_name: 'User'
-  
-    def build_saving(user, current)
-      return false if user == current
-  
-      return false unless Following.find_by(follower: current, followee: user).nil?
-  
-      self.follower = current
-      self.followee = user
-      save
-    end
-  end
+end
