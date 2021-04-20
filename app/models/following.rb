52bd7d2eb1ed6e185a296d68b5d158fbe0d1e518
oluwadare <wintan1418@gmail.com>
2021-04-20 11:52:32 +0100

    add valiations and methods for following functionality

--- app/models/following.rb
+++ app/models/following.rb
@@ -1,2 +1,15 @@
 class Following < ApplicationRecord
+    validates :follower, presence: true
+    validates :followee, presence: true
+    belongs to :follower, class_name: 'User'
+    belongs to :followee, class_name: 'User'
+
+    def build_saving(user,current)
+        return false if user == current
+
+        return false unless following.find_by(follower: current, followee: user).nil?
+        self.follower = current
+        self.followee =user
+        save
+    end
 end
