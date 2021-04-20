698f6f47e5042608dd41bb48aa35463058d88b14
oluwadare <wintan1418@gmail.com>
2021-04-20 12:31:55 +0100

    add methods for opinions functionality

--- app/controllers/opinions_controller.rb
+++ app/controllers/opinions_controller.rb
@@ -1,2 +1,17 @@
 class OpinionsController < ApplicationController
+    before_action :set_opinion, only: %i[show update edit destroy retweet]
+    before_action :authenticate_user!, except: %i[index show]
+    
+    def index
+        if current_user
+          @opinions = current_user.followees_opinions
+          @users = current_user.who_follow
+        else
+          @opinions = Opinion.ordered_opinion.include_user_copied
+          @users = User.ordered_users
+        end
+        @opinion = Opinion.new
+      end
+
+
 end
--- app/views/users/copy.rb
+++ app/views/users/copy.rb
@@ -1,30 +1,74 @@
-class SessionsController < ApplicationController
-  before_action :set_user, only: %i[create]
+class OpinionsController < ApplicationController
+  before_action :set_opinion, only: %i[show edit update destroy retweet]
+  before_action :authenticate_user!, except: %i[index show]
 
-  def new; end
+  def index
+    if current_user
+      @opinions = current_user.followeds_opinions
+      @users = current_user.who_follow
+    else
+      @opinions = Opinion.ordered_opinion.include_user_copied
+      @users = User.ordered_users
+    end
+    @opinion = Opinion.new
+  end
+
+  def show; end
+
+  def new
+    @opinion = Opinion.new
+  end
+
+  def edit
+    redirect_to opinions_path unless current_user == @opinion.user
+  end
 
   def create
-    if @user
-      session[:user_id] = @user.id
-      session[:username] = @user.username
-      redirect_to root_url, notice: 'Logged in!'
-    else
-      flash.now[:alert] = 'Username invalid'
-      render 'new'
+    @opinion = current_user.opinions.build(opinion_params)
+    @opinions = current_user.followeds_opinions
+    @users = current_user.who_follow
+
+    respond_to do |format|
+      if @opinion.save
+        format.html { redirect_to opinions_path, notice: 'Opinion was successfully created.' }
+      else
+        format.html { render :index }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @opinion.update(opinion_params)
+        format.html { redirect_to opinions_path, notice: 'Opinion was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
     end
   end
 
   def destroy
-    session[:user_id] = nil
-    session[:username] = nil
-    redirect_to root_url, notice: 'Logged out!'
+    @opinion.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referer, notice: 'Opinion was successfully deleted.' }
+    end
+  end
+
+  def retweet
+    current_user.copy_opi(@opinion)
+    redirect_to opinions_path
   end
 
   private
 
-  def set_user
-    @user = User.find_by_username(params[:username])
+  def set_opinion
+    @opinion = Opinion.find(params[:id])
   rescue ActiveRecord::RecordNotFound
-    @user = nil
+    flash[:alert] = 'Nonexistent post id'
+    redirect_to opinions_path
+  end
+
+  def opinion_params
+    params.require(:opinion).permit(:text)
   end
 end
